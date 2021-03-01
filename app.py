f039a694f83de3acc7fe587951f1a7c21501c58c
Saksham <sakshamconsul@gmail.com>
2021-03-01 01:40:52 +0100

    CORS attempt 2

--- app.py
+++ app.py
@@ -423,7 +423,7 @@ if __name__ == '__main__':
         }
     }
 
-    cherrypy.config.update({'server.socket_host': 'becooless.herokuapp.com', 'cors.expose.on': True,})
-    # cherrypy.config.update(
-    #     {'server.socket_port': int(os.environ.get('PORT', '6789'))})
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'cors.expose.on': True,})
+    cherrypy.config.update(
+        {'server.socket_port': int(os.environ.get('PORT', '8080'))})
     cherrypy.quickstart(Root(), '/', conf)
